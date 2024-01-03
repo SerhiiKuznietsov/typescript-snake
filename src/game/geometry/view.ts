1cf6ac935b9d80922cc54b4f3f5ec75832339a23
Serega <sergeykyz19998@gmail.com>
2024-01-03 15:30:03 +0200

    removed old logic

--- src/game/geometry/view.ts
+++
@@ -1,7 +0,0 @@
-export class View {
-  private _color: string;
-
-  constructor(color: string = 'black') {
-    this._color = color;
-  }
-}
--- src/game/utils/identify.ts
+++
@@ -1,5 +0,0 @@
-export class Identify {
-  public static createID(num1: number, num2: number): string {
-    return `${num1}-${num2}`;
-  }
-}
