fd6ae9a498a989c7e897d278ed7d63aa15e7719a
Serega <sergeykyz19998@gmail.com>
2024-05-10 17:51:13 +0300

    fix

--- src/game/geometry/shape/triangle.ts
+++ src/game/geometry/shape/triangle.ts
@@ -3,11 +3,11 @@ import { Shape } from './shape';
 import { Vector2 } from '../vector2';
 
 export class Triangle extends Shape {
-  constructor(public size: number, color: Color) {
-    super(color);
+  constructor(public size: number) {
+    super();
   }
 
-  draw(ctx: CanvasRenderingContext2D, position: Vector2): void {
+  draw(ctx: CanvasRenderingContext2D, position: Vector2, color: Color): void {
     ctx.beginPath();
     ctx.moveTo(position.x, position.y);
     ctx.lineTo(position.x + this.size, position.y);
@@ -16,7 +16,7 @@ export class Triangle extends Shape {
       position.y - this.size * Math.sin(Math.PI / 3)
     );
     ctx.closePath();
-    ctx.fillStyle = this.color.value;
+    ctx.fillStyle = color.value;
     ctx.fill();
   }
 }
