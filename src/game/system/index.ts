3e2727aedf6634a08fe62ebd5256244a95579af0
Serega <sergeykyz19998@gmail.com>
2024-12-09 15:58:58 +0200

    initialization system is in order

--- src/game/system/index.ts
+++ src/game/system/index.ts
@@ -1,27 +1,18 @@
 import { World } from '@/ecs/World';
 import { GameConfig } from '../config/game';
 import { Board } from '../board';
-import { DirectionControlSystem } from './directionControlSystem';
-import { MovementSystem } from './movementSystem';
-import { TeleportSystem } from './teleportSystem';
-import { CollisionSystem } from './collisionSystem';
-import { AttackSystem } from './attackSystem';
-import { DamageSystem } from './damageSystem';
-import { HealthSystem } from './healthSystem';
-import { SpawnSystem } from './spawnSystem';
-import { RenderSystem } from './renderSystem';
+import { PlayerInputSystem } from './PlayerInputSystem';
+import { MovementSystem } from './MovementSystem';
+import { CollisionSystem } from './CollisionSystem';
+import { RenderSystem } from './RenderSystem';
+
 // import { TailGrowthSystem } from './tailGrowthSystem';
 // import { FollowSystem } from './followSystem';
 
 export const initSystems = (world: World, config: GameConfig, board: Board) => {
   world
-    .addSystem(new DirectionControlSystem(world))
+    .addSystem(new PlayerInputSystem(world))
     .addSystem(new MovementSystem(world))
-    .addSystem(new TeleportSystem(config, world))
     .addSystem(new CollisionSystem(world))
-    .addSystem(new AttackSystem(world))
-    .addSystem(new DamageSystem(world))
-    .addSystem(new HealthSystem(world))
-    .addSystem(new SpawnSystem(world))
-    .addSystem(new RenderSystem(board, world));
+    .addSystem(new RenderSystem(world, board));
 };
